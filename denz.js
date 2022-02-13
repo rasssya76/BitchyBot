71664d6e225362b27fb120b3da7da263880cacf0
rasssya76 <80687852+rasssya76@users.noreply.github.com>
2022-02-13 16:40:34 +0700

    Add files via upload

--- denz.js
+++ denz.js
@@ -282,8 +282,7 @@ try {
 		const args = body.trim().split(/ +/).slice(1)
 		const isCmd = body.startsWith(prefix)
 		const arg = budy.slice(command.length + 2, budy.length)
-		const c = args.join(' ')
-		const q = args.join(' ')
+		const c = args.join(' ')		
 		var pes = (type === 'conversation' && mek.message.conversation) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text ? mek.message.extendedTextMessage.text : ''
 		const messagesD = pes.slice(0).trim().split(/ +/).shift().toLowerCase()
 		const dfrply = fs.readFileSync('./denz.jpg')
